caea9a41b1a689d0c179f52db3c6525d3fd97255
Hedda Olimb <holimb99@hotmail.com>
2025-05-14 19:34:41 +0200

    endringer forside

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
       </Head>
       <div className={styles.container}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Hi, my name is Hedda Olimb.</h1>
-          <p className={styles.subtitle}>Welcome to my page</p>
+          <h1 className={styles.title}>Hi, I'm Hedda Olimb.</h1>
+          <p className={styles.subtitle}>Welcome to my website!</p>
           <button className={styles.exploreBtn}>Explore</button>
         </div>
       </div>
