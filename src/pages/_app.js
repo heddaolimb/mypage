38c03262bed2e7a1939d01ab46fab5d4abbbd830
Hedda Olimb <holimb99@hotmail.com>
2025-08-18 23:18:08 +0200

    hero waves

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,28 +19,37 @@ export default function App({ Component, pageProps }) {
 
       {/* Wave-bakgrunn */}
       <div className="wave-bg">
-        <svg viewBox="0 0 1440 600" className="w-full h-full">
+        <svg
+          viewBox="0 0 2880 600"
+          className="wave-scroll"
+          preserveAspectRatio="none"
+        >
+          {/* Bølge nr 1 */}
           <path
             fill="#6366f1"
             fillOpacity="0.3"
             d="
-                M0,300 
-                C360,150,1080,450,1440,300 
-                L1440,600 L0,600Z
-              "
-          >
-            <animate
-              attributeName="d"
-              dur="22s"
-              repeatCount="indefinite"
-              values="
-                  M0,300 C360,120,1080,480,1440,300 L1440,600 L0,600Z;
-                  M0,350 C360,200,1080,400,1440,350 L1440,600 L0,600Z;
-                  M0,280 C360,100,1080,500,1440,280 L1440,600 L0,600Z;
-                  M0,300 C360,120,1080,480,1440,300 L1440,600 L0,600Z
-                "
-            />
-          </path>
+        M0,450 
+        C240,220, 480,420, 720,320
+        C960,220, 1200,420, 1440,320
+        C1680,220, 1920,420, 2160,320
+        C2400,220, 2640,420, 2880,320
+        L2880,600 L0,600Z
+      "
+          />
+          {/* Bølge nr 2 (kopi, flyttet til høyre) */}
+          <path
+            fill="#6366f1"
+            fillOpacity="0.3"
+            d="
+        M2880,450 
+        C3120,220, 3360,420, 3600,320
+        C3840,220, 4080,420, 4320,320
+        C4560,220, 4800,420, 5040,320
+        C5280,220, 5520,420, 5760,320
+        L5760,600 L2880,600Z
+      "
+          />
         </svg>
       </div>
 
