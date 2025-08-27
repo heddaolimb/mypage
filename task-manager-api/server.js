dfe522b13455e1c315290d218a223d9a25452f81
Hedda Olimb <holimb99@hotmail.com>
2025-08-27 17:55:19 +0200

    litt endringer render

--- task-manager-api/server.js
+++ task-manager-api/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB kobling (bytt connection string til din)
-mongoose.connect("mongodb://localhost:27017/taskmanager", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -48,6 +48,5 @@ app.delete("/api/tasks/:id", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () =>
-  console.log("Task Manager API running on http://localhost:5000")
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Task Manager API running on port ${PORT}`));
