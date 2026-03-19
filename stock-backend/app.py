21d3171ed0c9bba207626816cab548bc5ee069be
Hedda Olimb <holimb99@hotmail.com>
2026-03-19 16:23:23 +0100

    render fix

--- stock-backend/app.py
+++ stock-backend/app.py
@@ -18,5 +18,9 @@ def get_stock():
 
     return jsonify(data.to_dict(orient="records"))
 
+# RENDER
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
